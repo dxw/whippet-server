dc48d70bd71058ffba2d9bb332a73d246f9467d9
Harry Metcalfe <harry@thedextrousweb.com>
2013-04-08 22:47:47 +0100

    Load fallback mime.types if available (Fixes #21)

--- whippet.php
+++ whippet.php
@@ -54,10 +54,18 @@ if(!empty($argv[0])) {
 
 // Is there a mime.types file?
 if(!file_exists($options['mime-file'])) {
-  die_with_error(
-    "Unable to find file {$options['mime-file']}", 
-    "You can obtain the most recent mime file here:\n\n  http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types\n\nInstruct Whippet to use this file using the --mime-file argument.");
+  $local_fallback = dirname(__FILE__) . '/etc/mime.types';
+
+  if(!file_exists($options['mime-file'])) {
+    die_with_error(
+      "Unable to find file {$options['mime-file']}, and failed to load fallback", 
+      "You can obtain the most recent mime file here:\n\n  http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types\n\nInstruct Whippet to use this file using the --mime-file argument, or save it to /etc/mime.types");
+  }
+  else {
+    $options['mime-file'] = $local_fallback;
+  }
 }
+    
 
 // Is the specified port sane?
 if(!preg_match('/\d+/', $options['p']) || $options['p'] > 65536 || $options['p'] < 1) {
